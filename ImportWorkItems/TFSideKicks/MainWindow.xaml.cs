f940bac1db5ce8b5c64d108f6fb0046d5103af10
chriskowk <guoshaoyue@hotmail.com>
2019-08-22 18:15:29 +0800

    LastWriteTime

--- ImportWorkItems/TFSideKicks/MainWindow.xaml.cs
+++ ImportWorkItems/TFSideKicks/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace TFSideKicks
 
             string location = Assembly.GetExecutingAssembly().Location;
             FileInfo fi = new FileInfo(location);
-            this.Title = string.Format("导入工作项 Built-{0:yyyyMMdd.HH.mm}", fi.CreationTime);
+            this.Title = string.Format("导入工作项 Built-{0:yyyyMMdd.HH.mm}", fi.LastWriteTime);
 
             //运行路径（E:\VSTS\VS2015\ImportWorkItems\TFSideKicks\bin\Debug）下必须存在如下文件：Microsoft.WITDataStore64.dll，否则报错。另外“生成”Any CPU；去掉勾选“首选32位”选项
             WorkItemStore workItemStore = tpc.GetService<WorkItemStore>();
