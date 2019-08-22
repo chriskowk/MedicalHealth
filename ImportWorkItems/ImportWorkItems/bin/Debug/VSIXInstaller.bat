9498bc964e63bb200c46ddc9edd6a274f52a6a64
chriskowk <guoshaoyue@hotmail.com>
2019-08-22 09:41:22 +0800

    bat

---
+++ ImportWorkItems/ImportWorkItems/bin/Debug/VSIXInstaller.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" D:\CHRISµÄÎÄµµ\GitHub\MedicalHealth\ImportWorkItems\ImportWorkItems\bin\Debug\ImportWorkItems.vsix
+pause
