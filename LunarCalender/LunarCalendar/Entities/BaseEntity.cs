a5138e518f0ce3a9fb2aec8fe870beae08044e71
chriskowk <guoshaoyue@hotmail.com>
2021-01-05 17:00:45 +0800

    增加双向绑定通知机制

--- LunarCalender/LunarCalendar/Entities/BaseEntity.cs
+++ LunarCalender/LunarCalendar/Entities/BaseEntity.cs
@@ -1,14 +1,25 @@
 ﻿using Dapper.Contrib.Extensions;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LunarCalendar.Entities
 {
-    public class BaseEntity
+    public class BaseEntity : INotifyPropertyChanged
     {
+        private int _id;
         [Key]
-        public int ID { get; set; }
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; OnPropertyChanged("ID"); }
+        }
+        protected internal virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- LunarCalender/LunarCalendar/Entities/Diary.cs
+++ LunarCalender/LunarCalendar/Entities/Diary.cs
@@ -11,19 +11,89 @@ namespace LunarCalendar.Entities
     [Table("Diary")]
     public class Diary : BaseEntity
     {
-        public string Title { get; set; }
-        public string Keywords { get; set; }
-        public string Content { get; set; }
-        public DateTime RecordDate { get; set; }
-        public bool IsRemindRequired { get; set; }
-        public string JobGroup { get; set; }
-        public string JobName { get; set; }
-        public string JobTypeFullName { get; set; }
-        public string CronExpress { get; set; }
-        public DateTime? RunningStart { get; set; }
-        public DateTime? RunningEnd { get; set; }
-        public DateTime RowVersion { get; set; }
-        public bool IsDeleted { get; set; }
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; OnPropertyChanged("Title"); }
+        }
+        private string _keywords;
+        public string Keywords
+        {
+            get { return _keywords; }
+            set { _keywords = value; OnPropertyChanged("Keywords"); }
+        }
+        private string _content;
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; OnPropertyChanged("Content"); }
+        }
+        private DateTime _recordDate;
+        public DateTime RecordDate
+        {
+            get { return _recordDate; }
+            set { _recordDate = value; OnPropertyChanged("RecordDate"); }
+        }
+        private string _jobGroup;
+        public string JobGroup
+        {
+            get { return _jobGroup; }
+            set { _jobGroup = value; OnPropertyChanged("JobGroup"); }
+        }
+        private string _jobName;
+        public string JobName
+        {
+            get { return _jobName; }
+            set { _jobName = value; OnPropertyChanged("JobName"); }
+        }
+        private string _jobTypeFullName;
+        public string JobTypeFullName
+        {
+            get { return _jobTypeFullName; }
+            set { _jobTypeFullName = value; OnPropertyChanged("JobTypeFullName"); }
+        }
+        private DateTime? _runningStart;
+        public DateTime? RunningStart
+        {
+            get { return _runningStart; }
+            set { _runningStart = value; OnPropertyChanged("RunningStart"); }
+        }
+        private DateTime? _runningEnd;
+        public DateTime? RunningEnd
+        {
+            get { return _runningEnd; }
+            set { _runningEnd = value; OnPropertyChanged("RunningEnd"); }
+        }
+        private DateTime _rowVersion;
+        public DateTime RowVersion
+        {
+            get { return _rowVersion; }
+            set { _rowVersion = value; OnPropertyChanged("RowVersion"); }
+        }
+        private bool _isDeleted;
+        public bool IsDeleted
+        {
+            get { return _isDeleted; }
+            set { _isDeleted = value; OnPropertyChanged("IsDeleted"); }
+        }
+        private bool _isRemindRequired;
+        public bool IsRemindRequired
+        {
+            get { return _isRemindRequired; }
+            set { _isRemindRequired = value; OnPropertyChanged("IsRemindRequired"); }
+        }
+        private string _cronExpress;
+        public string CronExpress
+        {
+            get { return _cronExpress; }
+            set
+            {
+                _cronExpress = value.Trim();
+                IsRemindRequired = !string.IsNullOrEmpty(_cronExpress);
+                OnPropertyChanged("CronExpress");
+            }
+        }
     }
 
 }
