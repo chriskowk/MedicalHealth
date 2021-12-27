7d358573fa87d6cd1311e41474c995962ff70eb8
chriskowk <chriskwok@126.com>
2021-12-27 15:23:52 +0800

    aaa

--- ImportWorkItems/TFSideKicks/Menu.xaml.cs
+++ ImportWorkItems/TFSideKicks/Menu.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +24,93 @@ namespace TFSideKicks
         public Menu()
         {
             InitializeComponent();
+            //bool ret = CheckIdentifyCard("350501202202297032");
+        }
+
+        private static DateTime? GetDateTime(int year, int month, int day)
+        {
+            DateTime? dt;
+            try
+            {
+                dt = new DateTime(year, month, day);
+                return dt;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                return null;
+            }
+
+        }
+
+        private static string _IdPattern18 = @"^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$";
+        private static string _IdPattern15 = @"^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}[0-9Xx]$";
+        public static bool CheckIdentifyCard(string idCardStr)
+        {
+            if (idCardStr.Length == 15)
+            {
+                if (!Regex.IsMatch(idCardStr, _IdPattern15))
+                {
+                    MessageBox.Show("15位身份证号不符合标准", "提示");
+                    return false;
+                }
+
+                int year = int.Parse(idCardStr.Substring(6, 2)) + 1900;
+                int month;
+                int day;
+                string monthStr = idCardStr.Substring(8, 2);
+                string dayStr = idCardStr.Substring(10, 2);
+                if (monthStr.Substring(0, 1) == "0")
+                {
+                    monthStr = monthStr.Substring(1, 1);
+                }
+                if (dayStr.Substring(0, 1) == "0")
+                {
+                    dayStr = dayStr.Substring(1, 1);
+                }
+                month = int.Parse(monthStr);
+                day = int.Parse(dayStr);
+                if (GetDateTime(year, month, day) == null)
+                {
+                    MessageBox.Show("15位身份证号不符合标准", "提示");
+                    return false;
+                }
+            }
+            else if (idCardStr.Length == 18)
+            {
+                if (!Regex.IsMatch(idCardStr, _IdPattern18))
+                {
+                    MessageBox.Show("18位身份证号不符合标准", "提示");
+                    return false;
+                }
+
+                int year = int.Parse(idCardStr.Substring(6, 4));
+                int month;
+                int day;
+                string monthStr = idCardStr.Substring(10, 2);
+                string dayStr = idCardStr.Substring(12, 2);
+                if (monthStr.Substring(0, 1) == "0")
+                {
+                    monthStr = monthStr.Substring(1, 1);
+                }
+                if (dayStr.Substring(0, 1) == "0")
+                {
+                    dayStr = dayStr.Substring(1, 1);
+                }
+                month = int.Parse(monthStr);
+                day = int.Parse(dayStr);
+                if (GetDateTime(year, month, day) == null)
+                {
+                    MessageBox.Show("18位身份证号不符合标准", "提示");
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("身份证号必须为15或18位", "提示");
+                return false;
+            }
+
+            return true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
