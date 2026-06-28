c8ae26079eb77d2508fddfc7ef7fd1d12948777f
chriskowk <54020296+chriskowk@users.noreply.github.com>
2026-06-28 14:00:20 +0800

    TFSideKicks2020
    TFSideKicks2020

--- ImportWorkItems/ImportWorkItems/bin/Debug/VSIXInstaller.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" D:\CHRISµÄÎÄµµ\GitHub\MedicalHealth\ImportWorkItems\ImportWorkItems\bin\Debug\ImportWorkItems.vsix
-pause
